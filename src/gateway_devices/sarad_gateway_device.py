71f81be3983a72b3dd2c2f3bbc732d54ecb38628
Restyled.io <commits@restyled.io>
2021-04-17 05:21:48 +0000

    Restyled by black

--- src/gateway_devices/sarad_gateway_device.py
+++ src/gateway_devices/sarad_gateway_device.py
@@ -26,7 +26,8 @@ class SaradGatewayDevice(GenericGatewayDevice):
         self.__cluster = SaradCluster()
         try:
             self.__devi = self.__cluster.update_connected_instruments(
-                [self.get_serial_port()])
+                [self.get_serial_port()]
+            )
         except Exception:
             logger.error(f"USB Device Access Failed {device}")
             pass
@@ -35,8 +36,9 @@ class SaradGatewayDevice(GenericGatewayDevice):
     def get_serial_id(self):
 
         if len(self.__devi) == 1:
-            return "{}:{}".format(self.device.get("ID_MODEL", ""),
-                                  self.__devi[0].get_id())
+            return "{}:{}".format(
+                self.device.get("ID_MODEL", ""), self.__devi[0].get_id()
+            )
         return self.device.get("ID_SERIAL", "")
 
     def get_properties(self):
